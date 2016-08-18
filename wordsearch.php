5c73b0f8aef42b629453ab9a5e7a87a7565e9e37
mattposton <matt.poston@gmail.com>
2016-08-18 13:54:50 -0400

    added php boilerplate

---
+++ wordsearch.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+	
+	
+	<title>Wordsearch Generator</title>
+	
+	<!-- stylesheet(s) -->
+
+	<link rel="stylesheet" type="text/css" href="wordsearch.css">
+
+</head>
+
+<body>
+   <?php
+   
+    
+   
+   ?>
+    
+  
+</body>
